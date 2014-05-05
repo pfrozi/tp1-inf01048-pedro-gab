4a06bbdb8fdb04db4c12a49dd6c0c070a4ae056d
gstding@gmail.com <gstding@gmail.com@6497a250-c507-7378-7f55-9dc6d45364df>
2014-05-05 07:25:43 +0000

    final, sem comentarios

--- bot/config.js
+++ bot/config.js
@@ -25,7 +25,7 @@ var self = exports.config = {
 	                [8, 9, 10, 11, 12, 13, 14, 15],
 	                [0, 1, 2, 3, 4, 5, 6, 7]],
 	infinity : 1000000000000,
-	searchTimeOut : 4990,
+	searchTimeOut : 5500,
 	enPassantCode : 33,
 	emptySquareCode : 46,
 	piecesCodeMap : {
